4183a43773bbde6562a987e596f8c643f445db58
Bilal Oulahal <Bilal.OULAHAL@etu.uca.fr>
2025-02-28 20:38:55 +0100

    chore(commitlint): enable more type of commits

--- .commitlintrc.ts
+++ .commitlintrc.ts
@@ -1,10 +1,10 @@
 import type { UserConfig } from "@commitlint/types";
 
 const Configuration: UserConfig = {
-    extends: ["@commitlint/config-conventional"]
-    // rules: {
-    //   // ...
-    // },
+    extends: ["@commitlint/config-conventional"],
+    rules: {
+        "type-enum": [2, "always", ["build", "chore", "ci", "docs", "feat", "fix", "perf", "refactor", "revert", "style", "test", "tweak"]],
+    },
 };
 
 export default Configuration;
