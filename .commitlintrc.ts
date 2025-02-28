8db62730cd0fff05985203f561f44d977d018fc0
Bilal Oulahal <Bilal.OULAHAL@etu.uca.fr>
2025-02-28 21:10:15 +0100

    revert(prettier): change prettier rules & clean files

--- .commitlintrc.ts
+++ .commitlintrc.ts
@@ -3,8 +3,8 @@ import type { UserConfig } from "@commitlint/types";
 const Configuration: UserConfig = {
     extends: ["@commitlint/config-conventional"],
     rules: {
-        "type-enum": [2, "always", ["build", "chore", "ci", "docs", "feat", "fix", "perf", "refactor", "revert", "style", "test", "tweak"]],
-    },
+        "type-enum": [2, "always", ["build", "chore", "ci", "docs", "feat", "fix", "perf", "refactor", "revert", "style", "test", "tweak"]]
+    }
 };
 
 export default Configuration;
